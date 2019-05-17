d41e3c8c13fb0009d6ad2ddae156789d01a10bba
davy4325 <davy4325@126.com>
2019-05-17 09:22:57 +0800

    public code

--- demo/assets/scripts/Game.js
+++ demo/assets/scripts/Game.js
@@ -69,12 +69,12 @@ cc.Class({
         this.rivalScore = 0;
     },
 
-    onStartButton: function(event){
+    startPlayer: function(event){
         this.player.getComponent('Player').startJump();
         this.player.getComponent('Player').initKeyEvent();
         this.startButton.active = false;
     },
-
+    
     spawnNewStar: function() {
         // 使用给定的模板在场景中生成一个新节点
         var newStar = cc.instantiate(this.starPrefab);
@@ -102,6 +102,7 @@ cc.Class({
     },
 
     update: function (dt) {
+        this.net_data_callback();
         // 每帧更新计时器，超过限度还没有生成新的星星
         // 就会调用游戏失败逻辑
         if (this.timer > this.starDuration) {
@@ -132,5 +133,62 @@ cc.Class({
     gameOver: function () {
         this.player.stopAllActions(); //停止 player 节点的跳跃动作
         cc.director.loadScene('game');
-    }
+    },
+
+    onStartButton: function(event){
+        this.connect_to_server(); 
+    },
+
+    connect_to_server: function () {
+        pvp_utils.show_loading();
+        pvp_connect.instance().connect_to(pvp_public_msg.WAN_url);
+    },
+
+    net_data_callback: function () {
+        var msg_data = pvp_net_queue.instance().pop();
+        if(msg_data == null){
+           return;
+        }
+
+        switch(msg_data.cmd){
+            case pvp_public_msg.websocket_on_open:
+                this.websocket_on_open();
+                break;
+            case pvp_public_msg.websocket_on_error:
+                this.websocket_on_error();
+                break;
+            case pvp_public_msg.websocket_on_close:
+                this.websocket_on_close();
+                break;
+            default:
+                break;
+        }
+    },
+
+    websocket_on_close: function () {
+        cc.log("网络连接断开!");
+    },
+
+    websocket_on_error: function () {
+        pvp_utils.hide_loading();
+        pvp_utils.show_tips("无法连接服务器，请稍后再试!", 2);
+    },
+
+    websocket_on_open: function () {
+        var user_guid = "";
+        if(pvp_utils.guid_is_empty()){
+            user_guid = pvp_utils.uuid();
+            pvp_utils.save_guid(user_guid);
+        }else{
+            user_guid = pvp_utils.get_guid();
+        }
+
+        var name = pvp_utils.get_model();
+        var user_info_code = pvp_code.result_local_user_info(name, 10);
+
+        var obj_code = pvp_code.result_guid_type_value_info(pvp_private_msg.company_guid, pvp_private_msg.game_type, user_guid, 1000, user_info_code);
+
+        pvp_connect.instance().send_cmd(pvp_public_msg.public_msg_req_login, obj_code);
+    },
+
 });
